ca786d10fa01a86db90fa0933e8f6bd2edfa6b4f
JoelStansbury <48299585+JoelStansbury@users.noreply.github.com>
2019-06-20 09:50:57 -0400

    Add files via upload

--- Examples/example.py
+++ Examples/example.py
@@ -3,23 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from cod import cod
 from time import time
+from skimage.color import rgb2hsv
 
 def bbox(params):
     cx,cy,w,h = params
     x0,x1 = cx-w//2, cx+w//2
     y0,y1 = cy-h//2, cy+h//2
-    plt.plot([x0,x0],[y0,y1],color='r',lw=1)
-    plt.plot([x1,x1],[y0,y1],color='r',lw=1)
-    plt.plot([x0,x1],[y0,y0],color='r',lw=1)
-    plt.plot([x0,x1],[y1,y1],color='r',lw=1)
+    plt.plot([x0,x0,x1,x1,x0],
+             [y0,y1,y1,y0,y0],
+             # color='r',
+             lw=1)
 
-im = np.array(Image.open('test.png'))
-
+im1 = np.array(Image.open('test.png'))
+im = rgb2hsv(im1)
 t0 = time()
-objects = cod(im, Q = 2, eps=1,verbose=False)
-print(time()-t0)
 
-plt.imshow(im)
+# the black and white dial performs better with rgb
+# while the landscape performs better with hsv
+objects = cod(im1, Q = 2, eps=1,verbose=0)
+
+
+print(time()-t0)
+plt.imshow(im1)
 for i in objects:
     bbox(i)
 plt.xticks([])
